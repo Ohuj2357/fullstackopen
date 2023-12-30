8f9e028328e21e7e2cf99efa2fcd93a226436c05
root <root@LAPTOP-08D8K74H>
2023-12-30 16:25:16 +0200

    phonebook10/10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import personService from './services/personsS'
 import Persons from "./components/Persons"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
@@ -13,8 +13,8 @@ const App = () => {
   const [searchPerson, setSearch] = useState('')
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons')
+    personService
+      .getAll()
       .then(response => {
         setPersons(response.data)
       })
@@ -22,18 +22,26 @@ const App = () => {
 
   const addPerson = (event) => {
     event.preventDefault()
-    if (persons.map(person => person.name.toLowerCase()).indexOf(newName.toLowerCase())>-1){
-      alert(`${newName} is already added to phonebook`)
+    const personx = persons.find(p => p.name === newName)
+    if (personx!==undefined){
+      if (confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)){
+        const changedPerson = { ...personx, number: newNumber }
+        personService.update(personx.id, changedPerson).then(response => {
+          setPersons(persons.map(p => p.id !== personx.id ? p : response.data))
+        })
+      }
     }else{
       const personObject = {
         name: newName,
         number: newNumber,
-        id: persons.length + 1,
       }
-    
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+      personService
+        .create(personObject)
+        .then(response => {
+          setPersons(persons.concat(response.data))
+          setNewName('')
+          setNewNumber('')
+        })
     }
   }
 
@@ -58,7 +66,7 @@ const App = () => {
       nameHandler = {handleNameChange} numberHandler = {handleNumberChange} addPerson = {addPerson}/>
       
       <h2>Numbers</h2>
-      <Persons persons = {persons} search = {searchPerson} />
+      <Persons persons = {persons} search = {searchPerson} deletePerson = {personService.deletePerson} setPersons = {setPersons}/>
     </div>
   )
 }
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,15 +1,22 @@
 
-const Person = ({person}) => {
+const Person = ({person, deletePerson, setPersons, persons}) => {
+    const personDelete = () =>{
+      if (confirm(`Delete ${person.name}`)){
+        deletePerson(person.id).then(()=> setPersons(persons.filter(p => p.id !== person.id))).catch(error => {})
+      }
+    }
     return (
-      <div>{person.name} {person.number}</div>
+      <div>
+        {person.name} {person.number} <button onClick={personDelete}>delete</button>
+      </div>
     )
 }
 
-const Persons = ({persons, search}) => {
+const Persons = ({persons, search, deletePerson, setPersons}) => {
     return(
         <div>
         {persons.filter(person => person.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())).map(person => 
-          <Person key={person.id} person={person} />
+          <Person key={person.id} person={person} deletePerson = {deletePerson} setPersons = {setPersons} persons = {persons}/>
         )}
       </div>
     )
